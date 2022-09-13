6c8c080b05bed33498a17638d50e14e7316257a9
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-09-13 18:54:11 +0200

    Rewrite kernel build script

--- kernel_build.py
+++ kernel_build.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
-
+# CHROMEOS COMPILE INSTRUCTIONS: https://www.chromium.org/chromium-os/how-tos-and-troubleshooting/kernel-configuration/
 
 import os
 from shutil import rmtree as rmdir
+from shutil import copy as cp
 from pathlib import Path
 import sys
 import argparse
-from urllib.request import urlretrieve
 import subprocess as sp
 from os import system as bash
-from threading import Thread
-from urllib.error import URLError
-from time import sleep
+import time
 import json
 
 
 # parse arguments from the cli.
 def process_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--version', dest="version",
+    parser.add_argument(dest="version", type=str, nargs=1,
                         help="Kernel version to build(flag intended for docker containers)")
     parser.add_argument("-i", "--ignore-os", action="store_true", dest="ignore_os", default=False,
                         help="Allow building on non Ubuntu/debian based systems")
@@ -91,12 +89,71 @@ def patch(patch: str) -> str:
     return sp.run(f"git apply ../patches/{patch}", shell=True, capture_output=True).stderr.decode("utf-8").strip()
 
 
+def build_kernel() -> None:
+    print("\033[96m" + "Preparing to build kernel" + "\033[0m")
+    # prevent dirty kernel build
+    # add mod to .gitignore
+    with open(".gitignore", "a") as file:
+        file.write("mod")
+    # create .scmversion
+    with open(".scmversion", "w") as file:
+        file.write("")
+
+    # make config with default selections
+    print("\033[96m" + "Making config with default options" + "\033[0m")
+    bash("make olddefconfig")
+    # TODO: add config editing for users
+
+    print("\033[96m" + "Building kernel" + "\033[0m")
+    kernel_start = time.time()
+    if sp.run(f"make -j{cores}", shell=True).returncode == 2:
+        print("\033[91m" + f"Kernel build failed in: {time.time() - kernel_start}" + "\033[0m")
+        exit(1)
+    else:
+        print("\033[91m" + f"Kernel build succeeded in: {time.time() - kernel_start}" + "\033[0m")
+
+
+def build_modules() -> None:
+    print("\033[96m" + "Preparing for modules build" + "\033[0m")
+    rmdir("mod", ignore_errors=True)  # just in case
+    Path("mod").mkdir()
+    os.chdir("./mod")
+
+    print("\033[96m" + "Building modules" + "\033[0m")
+    modules_start = time.time()
+    if sp.run(f"make -j{cores} modules_install INSTALL_MOD_PATH=mod", shell=True).returncode == 2:
+        print("\033[91m" + f"Modules build failed in: {time.time() - modules_start}" + "\033[0m")
+        exit(1)
+    else:
+        print("\033[91m" + f"Modules build succeeded in: {time.time() - modules_start}" + "\033[0m")
+
+    print("\033[96m" + "Compressing modules" + "\033[0m")
+    # TODO: convert to one liner
+    # create extraction script
+    with open("fastxz", "w") as file:
+        file.write("xz -9 -T0")
+    bash("chmod +x fastxz")  # make script executable
+    modules_start = time.time()
+    if sp.run("tar -cvI './fastxz' -f ../modules.tar.xz lib/", shell=True).returncode == 2:
+        print("\033[91m" + f"Modules archival failed in: {time.time() - modules_start}" + "\033[0m")
+        exit(1)
+    else:
+        print("\033[91m" + f"Modules build succeeded in: {time.time() - modules_start}" + "\033[0m")
+    os.chdir("..")  # go back to chromeos kernel root
+
+
 if __name__ == "__main__":
     # Elevate script to root
     if os.geteuid() != 0:
         args = ['sudo', sys.executable] + sys.argv + [os.environ]
         os.execlpe('sudo', *args)
+    script_start = time.time()
     args = process_args()
+
+    # get number of cores
+    cores = sp.run("nproc", shell=True, capture_output=True).stdout.decode("utf-8").strip()
+    print(f"Available cores: {cores}")
+
     # check if running on ubuntu
     if not os.path.exists("/usr/bin/apt") and not args.ignore_os:  # check if running on ubuntu/debian
         print("This script is made for Ubuntu(docker). Use --ignore-os to run on other systems.\n"
@@ -106,7 +163,7 @@ if __name__ == "__main__":
         exit(1)
     if not args.version == "":
         print("Which kernel version would you like to use? ")
-        print("Manual building is not supported yet. Use old script for now.")
+        print("Manual building is not supported yet. Use the old script for now.")
         exit(1)
         # TODO: add version selection for users
     prepare_host()
@@ -116,13 +173,38 @@ if __name__ == "__main__":
     clone_kernel(read_kernel_head)
     create_boot_image()
     apply_patches()
+    build_kernel()
+    build_modules()
 
-    print("Preventing dirty kernel build")
-    # add mod to .gitignore
-    with open(".gitignore", "a") as file:
-        file.write("mod")
-    # create .scmversion
-    with open(".scmversion", "w") as file:
-        file.write("")
+    # determine file names
+    match args.version:
+        case "alt-chromeos-5.10":
+            modules_name = "modules-alt.tar.xz"
+            bzImage_name = "bzImage-alt"
+            # system_map_name = "System-alt.map"
+            config_name = "kernel-alt.config"
+        case "chromeos-5.15":
+            modules_name = "modules-exp.tar.xz"
+            bzImage_name = "bzImage-exp"
+            #  system_map_name = "System-exp.map"
+            config_name = "kernel-exp.config"
+        case "chromeos-5.10":
+            modules_name = "modules.tar.xz"
+            bzImage_name = "bzImage"
+            #  system_map_name = "System.map"
+            config_name = "kernel.config"
+        case _:
+            # shouldn't be built by GitHub actions
+            modules_name = "modules-old.tar.xz"
+            bzImage_name = "bzImage-old"
+            # system_map_name = "System-old.map"
+            config_name = "kernel-old.config"
 
+    # copy files to actual root
+    print("\033[96m" + "Copying files to actual root" + "\033[0m")
+    cp("modules.tar.xz", f"../{modules_name}")
+    cp("arch/x86/boot/bzImage", f"../{bzImage_name}")
+    # cp("System.map", f"../{system_map_name}")
+    cp(".config", f"../{config_name}")
 
+    print("\033[96m" + f"Build completed in: {time.time() - script_start}" + "\033[0m")
--- updatesubmodules.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd .. && git submodule foreach git pull origin main && git add . && git commit -m "Update submodules"; git push origin main
