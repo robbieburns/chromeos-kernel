427930aaca4a19d00c5c3408bef8d588990bfca2
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-09-15 00:18:43 +0200

    Extra debug stuff

--- kernel_build.py
+++ kernel_build.py
@@ -131,6 +131,8 @@ def build_modules() -> None:
 
     print("\033[96m" + "Building modules" + "\033[0m", flush=True)
     modules_start = time.time()
+    print(os.getcwd())
+    bash("ls -a")
     bash(f"make -j{cores} modules_install INSTALL_MOD_PATH=mod")
     # if sp.run(f"make -j{cores} modules_install INSTALL_MOD_PATH=mod", shell=True).returncode == 2:
     #     print("\033[91m" + f"Modules build failed in: {time.time() - modules_start}" + "\033[0m")
