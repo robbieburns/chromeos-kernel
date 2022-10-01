55d07d3e1348847d5e36fc0b1ce0d074bafd9e2c
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-10-02 01:55:03 +0200

    Improve code readability

--- kernel_build.py
+++ kernel_build.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # CHROMEOS COMPILE INSTRUCTIONS: https://www.chromium.org/chromium-os/how-tos-and-troubleshooting/kernel-configuration/
-
+# This script is primarily designed to be run in a cloud container system
 import os
 import sys
 from functions import *
+from functions import print_question as print_green
 import argparse
 import time
 import json
@@ -20,15 +21,15 @@ def process_args():
 
 
 def prepare_host() -> None:
-    print("\033[96m" + "Preparing host system" + "\033[0m", flush=True)
+    print_status("Preparing host system")
     bash("apt update -y")
     bash("apt install -y netpbm imagemagick git build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev "
          "bison binutils")
-    # TODO: add cleaning after previous builds
+    # TODO: add cleaning after previous builds for user builds
 
 
 def clone_kernel(kernel_head: str) -> None:
-    print("\033[96m" + "Cloning kernel: " + kernel_head + "\033[0m", flush=True)
+    print_status(f"Cloning kernel: {kernel_head}")
     if args.version == "alt-chromeos-5.10":
         bash("git clone --branch chromeos-5.10 --single-branch https://chromium.googlesource.com/chromiumos/third_party"
              "/kernel.git chromeos-kernel")
@@ -43,49 +44,51 @@ def clone_kernel(kernel_head: str) -> None:
 
 # Create boot icon/image
 def create_boot_image():
-    print("\033[96m" + "Creating boot image" + "\033[0m", flush=True)
+    print_status("Creating boot image")
     # TODO: Boot image
+    print_warning("Not yet implemented")
 
 
 def apply_patches():
-    print("\033[96m" + "Applying Eupnea patches" + "\033[0m", flush=True)
+    print_status("Applying Eupnea patches")
 
-    print("\033[96m" + "Applying bloog audio patch" + "\033[0m", flush=True)
+    print_status("Applying bloog audio patch")
     try:
         bash(f"git apply ../patches/bloog-audio.patch")
     except subprocess.CalledProcessError:
-        print("Bloog audio patch already applied", flush=True)
+        print_warning("Bloog audio patch already applied")
 
-    print("\033[96m" + "Applying important jsl i915 patch" + "\033[0m", flush=True)
+    print_status("Applying important jsl i915 patch")  # Add support for Jasperlake i915 (many DEDEDE devices)
     try:
         bash(f"git apply ../patches/jsl-i915.patch")
     except subprocess.CalledProcessError:
-        print("Checking if patch is already applied", flush=True)
+        print_status("Checking if patch is already applied")
         # check if patch is actually applied
         try:
             bash('grep -C3 "BIT(RCS0) | BIT(BCS0) | BIT(VCS0) | BIT(VECS0)" drivers/gpu/drm/i915/i915_pci.c | '
-                        'grep "jsl_info" -A5 | grep -c ".require_force_probe = 1"')
+                 'grep "jsl_info" -A5 | grep -c ".require_force_probe = 1"')
         except subprocess.CalledProcessError:
-            print("\033[91m" + "JSL i915 patch is not applied!! CRITICAL ERROR" + "\033[0m", flush=True)
+            print_error("JSL i915 patch is not applied!! CRITICAL ERROR")
         else:
-            print("Bloog audio patch already applied", flush=True)
+            print_status("Bloog audio patch already applied")
 
-    print("\033[96m" + "Applying headphone jack patch" + "\033[0m", flush=True)
+    print_status("Applying headphone jack patch")  # Fixes headphone jack detection
     try:
         bash(f"git apply ../patches/jack-detection.patch").strip()
     except subprocess.CalledProcessError:
-        print("Headphone jack patch already applied", flush=True)
+        print_warning("Headphone jack patch already applied")
 
-    print("\033[96m" + "Applying headphone jack utils patch" + "\033[0m", flush=True)
+    # Patch util functions that are not in the chromeos kernel, but are needed for the above patch to work
+    print_status("Applying headphone jack utils patch")
     try:
         bash(f"git apply ../patches/jack-detection-utils.patch").strip()
     except subprocess.CalledProcessError:
-        print("Headphone jack utils patch already applied", flush=True)
+        print_warning("Headphone jack utils patch already applied")
 
 
 def build_kernel() -> None:
-    print("\033[96m" + "Preparing to build kernel" + "\033[0m", flush=True)
-    # prevent dirty kernel build
+    print_status("Preparing to build kernel")
+    # preventing dirty kernel build:
     # add mod to .gitignore
     with open(".gitignore", "a") as file:
         file.write("mod")
@@ -95,7 +98,7 @@ def build_kernel() -> None:
 
     # copy config file from GitHub
     try:
-        rmfile(".config", )
+        rmfile(".config")  # just in case
     except FileNotFoundError:
         pass
     if args.version == "alt-chromeos-5.10":
@@ -104,36 +107,36 @@ def build_kernel() -> None:
         cpfile("../kernel.conf", "./.config")
 
     # make config with default selections
-    print("\033[96m" + "Making config with default options" + "\033[0m", flush=True)
+    print_status("Making config with default options")
     bash("make olddefconfig")
-    # TODO: add config editing for users
+    # TODO: add config editing for normal users
 
-    print("\033[96m" + "Building kernel" + "\033[0m", flush=True)
+    print_status(f"Building {args.version} kernel")
     kernel_start = time.time()
     try:
         bash(f"make -j{cores}")
     except subprocess.CalledProcessError:
-        print("\033[91m" + f"Kernel build failed in: {time.time() - kernel_start}" + "\033[0m")
+        print_error(f"Kernel build failed in: " + "%.0f" % (time.time() - kernel_start) + "seconds")
         exit(1)
-    print("\033[96m" + f"Kernel build succeeded in: {time.time() - kernel_start}" + "\033[0m", flush=True)
+    print_green(f"Kernel build succeeded in: " + "%.0f" % (time.time() - kernel_start) + "seconds")
 
 
 def build_modules() -> None:
-    print("\033[96m" + "Preparing for modules build" + "\033[0m", flush=True)
+    print_status("Preparing for modules build")
     rmdir("mod")  # just in case
     mkdir("mod")
 
-    print("\033[96m" + "Building modules" + "\033[0m", flush=True)
+    print_status("Building modules")
     modules_start = time.time()
     try:
-        # INSTALL_MOD_STRIP=1 removes debug symbols -> reduces unpacked kernel modules size
+        # INSTALL_MOD_STRIP=1 removes debug symbols -> reduces unpacked kernel modules size from 1.2GB to 70MB
         bash(f"make -j{cores} modules_install INSTALL_MOD_PATH=mod INSTALL_MOD_STRIP=1")
     except subprocess.CalledProcessError:
-        print("\033[91m" + f"Modules build failed in: {time.time() - modules_start}" + "\033[0m")
+        print_error(f"Modules build failed in: " + "%.0f" % (time.time() - modules_start) + "seconds")
         exit(1)
-    print("\033[96m" + f"Modules build succeeded in: {time.time() - modules_start}" + "\033[0m", flush=True)
+    print_green(f"Modules build succeeded in: " + "%.0f" % (time.time() - modules_start) + "seconds")
 
-    print("\033[96m" + "Compressing modules" + "\033[0m", flush=True)
+    print_status("Compressing kernel modules")
     os.chdir("./mod")
     # TODO: convert to one liner
     # create extraction script
@@ -144,40 +147,40 @@ def build_modules() -> None:
     try:
         bash("tar -cvI './fastxz' -f ../modules.tar.xz lib/")
     except subprocess.CalledProcessError:
-        print("\033[91m" + f"Modules archival failed in: {time.time() - modules_start}" + "\033[0m")
+        print_error(f"Modules archival failed in: " + "%.0f" % (time.time() - modules_start) + "seconds")
         exit(1)
-    print("\033[96m" + f"Modules archival succeeded in: {time.time() - modules_start}" + "\033[0m", flush=True)
+    print_green(f"Modules archival succeeded in: " + "%.0f" % (time.time() - modules_start) + "seconds")
     os.chdir("..")  # go back to chromeos kernel root
 
 
 def build_headers():
-    print("\033[96m" + "Preparing for header build" + "\033[0m", flush=True)
+    print_status("Preparing for header build")
     rmdir("headers")  # just in case
     mkdir("headers")
 
-    print("\033[96m" + "Building headers" + "\033[0m", flush=True)
-    modules_start = time.time()
+    print_status("Building headers")
+    headers_start = time.time()
     try:
         bash(f"make -j{cores} headers_install INSTALL_HDR_PATH=headers")
     except subprocess.CalledProcessError:
-        print("\033[91m" + f"Headers build failed in: {time.time() - modules_start}" + "\033[0m")
+        print_error(f"Headers build failed in: " + "%.0f" % (time.time() - headers_start) + "seconds")
         exit(1)
-    print("\033[96m" + f"Headers build succeeded in: {time.time() - modules_start}" + "\033[0m", flush=True)
+    print_green(f"Headers build succeeded in: " + "%.0f" % (time.time() - headers_start) + "seconds")
 
-    print("\033[96m" + "Compressing headers" + "\033[0m", flush=True)
+    print_status("Compressing headers")
     os.chdir("./headers")
     # TODO: convert to one liner
     # create extraction script
     with open("fastxz", "w") as file:
         file.write("xz -9 -T0")
     bash("chmod +x fastxz")  # make script executable
-    modules_start = time.time()
+    headers_start = time.time()
     try:
         bash("tar -cvI './fastxz' -f ../headers.tar.xz include/")
     except subprocess.CalledProcessError:
-        print("\033[91m" + f"Headers archival failed in: {time.time() - modules_start}" + "\033[0m")
+        print_error(f"Headers archival failed in: " + "%.0f" % (time.time() - headers_start) + "seconds")
         exit(1)
-    print("\033[96m" + f"Headers archival succeeded in: {time.time() - modules_start}" + "\033[0m", flush=True)
+    print_green(f"Headers archival succeeded in: " + "%.0f" % (time.time() - headers_start) + "seconds")
     os.chdir("..")  # go back to chromeos kernel root
 
 
@@ -191,23 +194,23 @@ if __name__ == "__main__":
 
     # get number of cores
     cores = bash("nproc")
-    print(f"Available cores: {cores}", flush=True)
+    print_status(f"Available cpu cores: {cores}")
 
-    # check if running on ubuntu
-    if not path_exists("/usr/bin/apt") and args.ignore_os:  # check if running on ubuntu/debian
-        print("This script is made for Ubuntu(docker). Use --ignore-os to run on other systems.\n"
-              " Install these packages using your package manager:", flush=True)
-        print("netpbm imagemagick git build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison cgpt "
-              "vboot-kernel-utils", flush=True)
+    # check if running on ubuntu and no ignore-os flag
+    if not (path_exists("/usr/bin/apt") and args.ignore_os):
+        print_error("This script is made for Ubuntu(container). Use --ignore-os to run on other systems.\n"
+                    " Install these packages using your package manager:")
+        print_error("netpbm imagemagick git build-essential ncurses-dev xz-utils libssl-dev bc flex libelf-dev bison "
+                    "cgpt vboot-kernel-utils")
         exit(1)
     if args.version == "":
-        print("Which kernel version would you like to use? ", flush=True)
-        print("Manual building is not supported yet. Use the old script for now.", flush=True)
+        print_green("Which kernel version would you like to use? ")
+        print_error("Manual building is not supported yet. Use the old script for now.")
         exit(1)
         # TODO: add version selection for users
     if args.verbose:
-        print("\033[93m" + "Verbosity increased" + "\033[0m")
-        enable_verbose()  # enable verbose output in functions.py
+        print_warning("Verbosity increased")
+        set_verbose(args.verbose)  # enable verbose output in functions.py
 
     prepare_host()
     # get kernel_head
@@ -215,8 +218,9 @@ if __name__ == "__main__":
         with open("kernel_versions.json", "r") as json_file:
             read_kernel_head = json.load(json_file)[args.version]
     except KeyError:
-        print("\033[91m" + "Kernel version not available." + "\033[0m", flush=True)
+        print_error("Kernel version not available.")
         exit(1)
+
     clone_kernel(read_kernel_head)
     create_boot_image()
     apply_patches()
@@ -236,7 +240,7 @@ if __name__ == "__main__":
             bzImage_name = "bzImage-exp"
             modules_name = "modules-exp.tar.xz"
             headers_name = "headers-exp.tar.xz"
-            #  system_map_name = "System-exp.map"
+            # system_map_name = "System-exp.map"
             # config_name = "kernel-exp.config"
         case "chromeos-5.10":
             bzImage_name = "bzImage"
@@ -252,13 +256,12 @@ if __name__ == "__main__":
             # system_map_name = "System-old.map"
             # config_name = "kernel-old.config"
 
-    # copy files to actual root
-    print("\033[96m" + "Copying files to actual root" + "\033[0m", flush=True)
-
+    # copy files up one dir for artifact upload
+    print_status("Copying files to actual root")
     cpfile("arch/x86/boot/bzImage", f"../{bzImage_name}")
     cpfile("modules.tar.xz", f"../{modules_name}")
     cpfile("headers.tar.xz", f"../{headers_name}")
     # cp("System.map", f"../{system_map_name}")
     # cpfile(".config", f"../{config_name}")
 
-    print("\033[96m" + f"Full build completed in: {time.time() - script_start}" + "\033[0m", flush=True)
+    print_header(f"Full build completed in: " + "%.0f" % (time.time() - script_start) + "seconds")
