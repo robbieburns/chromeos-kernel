b75fa15ebcf4dc943681e0722f874bdbe6c1f779
Apacelus <46828271+Apacelus@users.noreply.github.com>
2022-10-23 10:40:12 +0000

    Fix path for tar

--- kernel_build.py
+++ kernel_build.py
@@ -157,7 +157,7 @@ def build_headers():
     os.chdir("./headers")
     headers_start = perf_counter()
     try:
-        bash("tar -cv -I 'xz -9 -T0' -f ../../headers.tar.xz ./")  # fast multicore xtreme compression
+        bash("tar -cv -I 'xz -9 -T0' -f ../headers.tar.xz ./")  # fast multicore xtreme compression
     except subprocess.CalledProcessError:
         print_error(f"Headers archival failed in: " + "%.0f" % (perf_counter() - headers_start) + " seconds")
         exit(1)
